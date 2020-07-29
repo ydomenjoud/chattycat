1ee0952d0df4b2e7f6f8f66427cb683a1ed55046
ydomenjoud <yannick@6tech.net>
2020-07-29 23:43:38 +0200

    upgrade dependencies

--- website/.eleventy.js
+++ website/.eleventy.js
@@ -1,55 +1,60 @@
 const Image = require("@11ty/eleventy-img");
 const slugify = require("slugify");
+const pluginSass = require("eleventy-plugin-sass");
 
 const distDir = '../dist/';
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
 
-  // add plugin for remote image
-  eleventyConfig.addNunjucksAsyncShortcode("remoteImage", async function(src, alt, options) {
-    // returns Promise
-    const defaultOptions = {
-      outputDir: distDir + 'assets/img/',
-      urlPath: "/assets/img/",
-    }
-    options = {...defaultOptions, ...options};
-    let stats = await Image(src, options);
-    let lowestSrc = stats.jpeg[0];
-    let sizes = "100vw"; // Make sure you customize this!
+    // add plugin for remote image
+    eleventyConfig.addNunjucksAsyncShortcode("remoteImage", async function (src, alt, options) {
+        // returns Promise
+        const defaultOptions = {
+            outputDir: distDir + 'assets/img/',
+            urlPath: "/assets/img/",
+        };
+        options = {...defaultOptions, ...options};
+        let stats = await Image(src, options);
+        let lowestSrc = stats.jpeg[0];
+        let sizes = "100vw"; // Make sure you customize this!
 
-    // Iterate over formats and widths
-    return `<picture>
+        // Iterate over formats and widths
+        return `<picture>
       ${Object.values(stats).map(imageFormat => {
-      return `  <source type="image/${imageFormat[0].format}" srcset="${imageFormat.map(entry => `${entry.url} ${entry.width}w`).join(", ")}" sizes="${sizes}">`;
-    }).join("\n")}
+            return `  <source type="image/${imageFormat[0].format}" srcset="${imageFormat.map(entry => `${entry.url} ${entry.width}w`).join(", ")}" sizes="${sizes}">`;
+        }).join("\n")}
         <img
           alt="${alt}"
           src="${lowestSrc.url}"
           width="${lowestSrc.width}"
           height="${lowestSrc.height}">
       </picture>`;
-  });
+    });
 
-  // replace default slug with better imp
-  eleventyConfig.addFilter("slug", (input) => {
-    const options = {
-      replacement: "-",
-      remove: /[&,+()$~%.'":*?<>{}]/g,
-      lower: true
+    // replace default slug with better imp
+    eleventyConfig.addFilter("slug", (input) => {
+        const options = {
+            replacement: "-",
+            remove: /[&,+()$~%.'":*?<>{}]/g,
+            lower: true
+        };
+        return slugify(input, options);
+    });
+
+    // add sass plugin
+    eleventyConfig.addPlugin(pluginSass);
+    
+    // copy assets
+    eleventyConfig.addPassthroughCopy("assets");
+
+
+    return {
+        dir: {
+            output: distDir,
+            input: "src"
+        },
+        templateFormats: ["njk"]
     };
-    return slugify(input, options);
-  });
-
-  // copy assets
-  eleventyConfig.addPassthroughCopy("assets");
-
-  return {
-    dir: {
-      output: distDir,
-      input: "src"
-    },
-    templateFormats: ["njk"]
-  };
 
 };
 
