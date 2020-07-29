77c4a8bf50499506f382aab7b5356071866ce2f2
ydomenjoud <yannick@6tech.net>
2020-07-29 23:49:30 +0200

    fix "no such file or directory, open '../dist/"

--- website/src/_data/global.js
+++ website/src/_data/global.js
@@ -1,21 +1,26 @@
 const https = require("https");
 const fs = require("fs");
 
-module.exports = async function () {
-  let options = new URL("https://europe-west1-chattycat-site.cloudfunctions.net/getBooks");
+const distDir = '../dist/';
 
-  return new Promise((resolve, reject) => {
-    https.get(options, res => {
-      let data = "";
-      res.on("data", d => data += d);
-      res.on("end", () => {
-        const books = JSON.parse(data);
-        // copy books to dist for action on website without external requests
-        fs.writeFileSync("../dist/books.json", data);
-        console.log(`found ${books.length} books`);
-        resolve({books})
-      });
-      res.on("error", error => reject(error));
+module.exports = async function () {
+    let options = new URL("https://europe-west1-chattycat-site.cloudfunctions.net/getBooks");
+
+    return new Promise((resolve, reject) => {
+        https.get(options, res => {
+            let data = "";
+            res.on("data", d => data += d);
+            res.on("end", () => {
+                const books = JSON.parse(data);
+                // copy books to dist for action on website without external requests
+                if (!fs.existsSync(distDir)) {
+                    fs.mkdirSync(distDir);
+                }
+                fs.writeFileSync(distDir + "books.json", data);
+                console.log(`found ${books.length} books`);
+                resolve({books});
+            });
+            res.on("error", error => reject(error));
+        });
     });
-  });
 };
