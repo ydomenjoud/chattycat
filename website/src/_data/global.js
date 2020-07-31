0513a0776666a462856b539617a21eb497b3a3cb
Pascal Besson <pascalbes@gmail.com>
2020-07-31 15:31:47 +0200

    premières empreintes et 5 nouveautés

--- website/src/_data/global.js
+++ website/src/_data/global.js
@@ -17,9 +17,15 @@ module.exports = async function () {
                     fs.mkdirSync(distDir);
                 }
                 fs.writeFileSync(distDir + "books.json", data);
-                console.log(`found ${books.length} books`);
+
+                //get last 5 published books
+                let newBooks = [...books]
+                newBooks  = newBooks.sort((a,b)=> (a.date._seconds - b.date._seconds < 0) ? 1 : -1).slice(0,5)
+                
+                console.log(`found ${books.length} books, including ${newBooks.length} novelties`);
                 resolve({
                     books,
+                    newBooks
                 });
             });
             res.on("error", error => reject(error));
